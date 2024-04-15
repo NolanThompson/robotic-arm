ddf720e408767e00be1f63310e4814f367711100
NolanThompson <j0thom42@louisville.edu>
2024-04-15 17:25:02 -0400

    initial commit

---
+++ VirtualJoystick.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RoboticArmUI
+{
+    public class VirtualJoystick : Control
+    {
+        private Point center;
+        private Point thumbPosition;
+        private bool thumbDragging;
+
+        public event EventHandler<Point> JoystickMoved;
+
+        public VirtualJoystick()
+        {
+            center = new Point(Width / 2, Height / 2);
+            thumbPosition = center;
+            DoubleBuffered = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // Calculate the smaller radius for the circle
+            int smallerRadius = Math.Min(Width, Height) / 4;
+
+            // Draw the smaller red background circle
+            int diameter = smallerRadius * 2;
+            g.FillEllipse(Brushes.Red, center.X - smallerRadius, center.Y - smallerRadius, diameter, diameter);
+
+            // Draw the thumb circle (20% of the size of the background circle)
+            int thumbRadius = smallerRadius / 4;
+            g.FillEllipse(Brushes.Gray, thumbPosition.X - thumbRadius, thumbPosition.Y - thumbRadius, thumbRadius * 2, thumbRadius * 2);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Left)
+            {
+                if (Math.Sqrt(Math.Pow(e.X - center.X, 2) + Math.Pow(e.Y - center.Y, 2)) <= (Width / 4))
+                {
+                    thumbPosition = e.Location;
+                    thumbDragging = true;
+                    Invalidate();
+                }
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (thumbDragging)
+            {
+                // Calculate the vector from the center to the mouse position
+                int deltaX = e.X - center.X;
+                int deltaY = e.Y - center.Y;
+
+                // Calculate the distance from the center to the mouse position
+                double distanceToCenter = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+                // If the distance is greater than the radius, limit it to the radius
+                if (distanceToCenter > Width / 4)
+                {
+                    double scaleFactor = (Width / 4) / distanceToCenter;
+                    deltaX = (int)(deltaX * scaleFactor);
+                    deltaY = (int)(deltaY * scaleFactor);
+                }
+
+                // Set the thumb position to the closest point on the circle to the mouse cursor
+                thumbPosition = new Point(center.X + deltaX, center.Y + deltaY);
+
+                Invalidate();
+                OnJoystickMoved(thumbPosition);
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (thumbDragging && e.Button == MouseButtons.Left)
+            {
+                thumbDragging = false;
+                thumbPosition = center;
+                Invalidate();
+                OnJoystickMoved(thumbPosition);
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            center = new Point(Width / 2, Height / 2);
+            thumbPosition = center;
+            Invalidate();
+        }
+
+        protected virtual void OnJoystickMoved(Point position)
+        {
+            JoystickMoved?.Invoke(this, position);
+        }
+    }
+}
---
+++ arduinoOut/sketch/MotorController.cpp
@@ -0,0 +1,34 @@
+#line 1 "C:\\Users\\gjold\\OneDrive\\Desktop\\Prototype Tank Stuff\\Analysis Scripts\\RoboticArmUI\\RoboticArmUI\\Arduino\\MotorController.cpp"
+#include "MotorController.h"
+#include <Arduino.h>
+
+MotorController::MotorController(int forwardPin, int reversePin) {
+    this->forwardPin = forwardPin;
+    this->reversePin = reversePin;
+    pinMode(forwardPin, OUTPUT);
+    pinMode(reversePin, OUTPUT);
+}
+
+void MotorController::setSpeed(float speed) {
+    // Map the speed value to the PWM range
+    int pwmValue = mapSpeedToPWM(speed);
+    
+    // Set PWM output based on the speed value
+    if (speed > 0) {
+        analogWrite(reversePin, 0);
+        analogWrite(forwardPin, pwmValue);
+    } else if (speed < 0) {
+        analogWrite(forwardPin, 0);
+        analogWrite(reversePin, pwmValue); // Make sure it's positive
+    } else {
+        // Stop the motor
+        analogWrite(forwardPin, 0);
+        analogWrite(reversePin, 0);
+    }
+}
+
+int MotorController::mapSpeedToPWM(float speed) {
+    // Map the speed range (-1 to 1) to the PWM range (0 to 255)
+    int pwmValue = constrain(int(abs(speed) * 255), 0, 255);
+    return pwmValue;
+}
